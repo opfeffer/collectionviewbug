1bbd795c81e0de7f27b22f1be2686954c295954f
Oliver Pfeffer <oliver.pfeffer@qvc.com>
2014-09-08 20:13:54 -0400

    Adding CollectionViews and DataSource methods

--- CollectionView/CVBViewController.m
+++ CollectionView/CVBViewController.m
@@ -8,7 +8,11 @@
 
 #import "CVBViewController.h"
 
-@interface CVBViewController ()
+@interface CVBViewController () <UICollectionViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView2;
+
 
 @end
 
@@ -18,6 +22,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
+        [self.collectionView2 registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return collectionView.tag==0 ? 4 : 3;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor colorWithRed:(50*indexPath.item)/255.f green:(50*indexPath.item)/255.f blue:1.0f alpha:1];
+    
+    return cell;
+}
+
 
 @end
